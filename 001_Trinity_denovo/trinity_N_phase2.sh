fc0d7666f960afc897fd0ddd0bc46b207eb93c29
kweir6 <94187126+kweir6@users.noreply.github.com>
2023-11-13 21:33:45 +0100

    Add files via upload

---
+++ 001_Trinity_denovo/trinity_N_phase2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH
+#SBATCH --job-name=Tri_distr_Job
+#SBATCH --partition=bigmem
+#SBATCH -A sblacks1_bigmem
+#SBATCH -t 02-00:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mail-type=end
+
+
+module load cuda/11.1.0 
+
+cd /home/kweir6
+
+singularity exec -e ./trinityrnaseq Trinity \
+          --seqType fq \
+          --samples_file /home/kweir6/data_sblacks1/Kurt/Hibernation/scripts/trinity_samples_bearded_dragon.txt \
+          --max_memory 300G --CPU 20 \
+          --output /home/kweir6/data_sblacks1/Kurt/Hibernation/Trinity_output/Trinity-out-dir-Bearded-Dragon \
+
